bdcd71e1dda8ed3498f66a3c7270162bddae755e
Udemy Rails Course Otavio Colella <otavio.colella@gmail.com>
2016-05-26 09:24:15 +0000

    Add production image upload

---
+++ app/models/image.rb
@@ -0,0 +1,16 @@
+class Image < ActiveRecord::Base
+  belongs_to :user
+  mount_uploader :picture, PictureUploader 
+  validate :picture_size
+  
+  
+  private
+  
+  def picture_size
+    if picture.size > 5.megabytes
+      errors.add(:picture, "Cannot be more then 5Mb")
+    end
+    
+  end
+  
+end
