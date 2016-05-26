a55ca3b15c3dd0a3cbad8cc05b076e2aeddd45be
Udemy Rails Course Otavio Colella <otavio.colella@gmail.com>
2016-05-26 10:58:42 +0000

    a new fix for region

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -8,9 +8,7 @@ if Rails.env.production?
   
       :aws_access_key_id => ENV['S3_ACCESS_KEY'],
   
-      :aws_secret_access_key => ENV['S3_SECRET_KEY'],
-      
-      :aws_region => ENV['AWS_REGION']
+      :aws_secret_access_key => ENV['S3_SECRET_KEY']
       
     }
   
