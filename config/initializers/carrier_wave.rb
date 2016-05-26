4ff17cb812d04aba4a9d644263b61df4b33a76b9
Udemy Rails Course Otavio Colella <otavio.colella@gmail.com>
2016-05-26 10:41:32 +0000

    fixing

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -8,8 +8,10 @@ if Rails.env.production?
   
       :aws_access_key_id => ENV['S3_ACCESS_KEY'],
   
-      :aws_secret_access_key => ENV['S3_SECRET_KEY']
-  
+      :aws_secret_access_key => ENV['S3_SECRET_KEY'],
+      
+      :aws_region => ENV['AWS_REGION']
+      
     }
   
     config.fog_directory = ENV['S3_BUCKET']
